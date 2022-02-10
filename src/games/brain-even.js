9fb9384b293711f696c4b5deb775cfed26b7a392
Daria Pavlova <dariatop1@gmail.com>
2022-02-10 23:49:30 +0300

    refactoring games, fix index.js

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -4,9 +4,9 @@ import engine from '../index.js';
 const findEven = () => {
   const number = getRandomInRange(0, 100);
   if (number % 2 === 0) {
-    return [number, 'yes'];
+    return [String(number), 'yes'];
   }
-  return [number, 'no'];
+  return [String(number), 'no'];
 };
 engine('Answer "yes" if the number is even, otherwise answer "no".', findEven);
 export default findEven;
