a4f90516f0bad0fad8438930a2f258f7d681db47
Daria Pavlova <dariatop1@gmail.com>
2022-02-13 16:40:13 +0300

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -3,22 +3,19 @@ import userGreeting from './cli.js';
 
 const userName = userGreeting();
 
-const engine = (gameDiscription, roundFunction) => {
-  let correctness = true;
+const engine = (gameDiscription, generateRound) => {
   const roundsCount = 3;
+  console.log(gameDiscription);
   for (let i = 0; i < roundsCount; i += 1) {
-    console.log(gameDiscription);
-    const response = roundFunction();
-    console.log(`Question: ${response[0]}`);
-    const expected = response[1];
+    const [question, expected] = generateRound();
+    console.log(`Question: ${question}`);
     const actual = readlineSync.question('Your answer: ');
     if (expected !== actual) {
-      correctness = false;
       console.log(`'${actual}' is wrong answer ;(. Correct answer was '${expected}'.\nLet's try again, ${userName}!`);
-      break;
+      return;
     }
     console.log('Correct!');
   }
-  if (correctness) console.log(`Congratulations, ${userName}!`);
+  console.log(`Congratulations, ${userName}!`);
 };
 export default engine;
